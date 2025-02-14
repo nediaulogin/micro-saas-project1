7a7404fe7bbd2b748b079b4cda771203d2e89738
nediaulogin <nediaulog@gmail.com>
2025-02-14 13:25:44 -0300

    This commit adds the email provider and settings needed for our magic link

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,5 +1,33 @@
 import NextAuth from "next-auth"
+import EmailProvider from "next-auth/providers/email"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "../databae"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    /*Nodemailer({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),*/
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
 })
